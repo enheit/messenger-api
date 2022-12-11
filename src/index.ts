1b8264b174ab9a8ab44a093de44425922a0cca6e
Roman Mahotskyi <roman.mahotskyi@gmail.com>
2022-12-11 23:28:00 +0200

    Add broken MongoDB docker compose file

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
 import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import express from 'express'
+import { MongoClient } from 'mongodb'
 
 import { authRouter } from './components/auth/auth.router.js'
 import { chatRouter } from './components/chat/chat.router.js'
 import { userRouter } from './components/user/user.router.js'
 
 dotenv.config()
+
+export const mongoClient = new MongoClient(process.env.DATABASE_URL as string)
+
+await mongoClient.connect()
+
+const db = mongoClient.db('messenger-db')
+
+const session = mongoClient.startSession()
+
+try {
+  await session.withTransaction(async () => {
+    await db.collection('users').insertOne({ name: 'Oleh' }, { session })
+
+    await db.collection('orders').insertOne({ orderFor: 'Oleh' }, { session })
+  })
+} catch (error) {
+  console.log(error)
+} finally {
+  await session.endSession()
+}
+
+await db.collection('users').insertOne({ name: 'Roman' })
 
 const app = express()
 
--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -4,6 +4,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       PORT: string | undefined
+      DATABASE_URL: string | undefined
     }
   }
 }
